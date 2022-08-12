2c7d11d2693b4ad5a9defdf78a44e9ea6f1b8100
Oğuzcan Turan <can.turan.10@gmail.com>
2022-08-12 09:58:51 +0300

    demo page updated and doc addded

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# satellighte-streamlit
-Implementation of Streamlit library for Satellighte
+<p align="right">
+    <a href="https://www.buymeacoffee.com/canturan10"><img src="https://img.buymeacoffee.com/button-api/?text=You can buy me a coffee&emoji=&slug=canturan10&button_colour=5F7FFF&font_colour=ffffff&font_family=Comic&outline_colour=000000&coffee_colour=FFDD00" width="200" /></a>
+</p>
+
+<!-- PROJECT SUMMARY -->
+<p align="center">
+    <img width="100px" src="https://raw.githubusercontent.com/canturan10/satellighte-streamlit/master/src/satellighte-streamlit.png" align="center" alt="satellighte-streamlit" />
+<h2 align="center">Satellighte-Streamlit</h2>
+<h4 align="center">Implementation of Streamlit Library for Satellighte</h4>
+
+<p align="center">
+    <strong>
+        <a href="https://share.streamlit.io/canturan10/satellighte-streamlit/app.py">Demo Page</a>
+        •
+        <a href="https://canturan10.github.io/satellighte/">Website</a>
+        •
+        <a href="https://satellighte.readthedocs.io/">Docs</a>
+    </strong>
+</p>
+
+<!-- ABOUT THE PROJECT -->
+## About The Satellighte-Streamlit
+
+**Satellighte-Streamlit** is a web application that is developed with the Streamlit library. With satellighte-streamlit, you can easily classify satellite images with the models trained with satellighte.
+
+<!-- LICENSE -->
+## License
+
+This project is licensed under `MIT` license. See [`LICENSE`](LICENSE) for more information.
+
+<!-- REFERENCES -->
+## References
+
+The references used in the development of the project are as follows.
+
+- [Satellighte](https://github.com/canturan10/satellighte)
+- [Streamlit](https://streamlit.io/)
+
+<!-- CITATIONS -->
+## Citations
+
+<details>
+  <summary>Click to expand!</summary>
+
+```bibtex
+@misc{Turan_satellighte,
+author = {Turan, Oguzcan},
+title = {{satellighte}},
+url = {https://github.com/canturan10/satellighte}
+}
+```
+
+</details>
+
+![-----------------------------------------------------](https://raw.githubusercontent.com/canturan10/readme-template/master/src/colored_4b.png)
+
+Give a ⭐️ if this project helped you!
--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import random
+from datetime import datetime
+
 import numpy as np
 import requests
 import satellighte as sat
@@ -8,44 +11,60 @@ from PIL import Image
 def main():
     # pylint: disable=no-member
 
+    st.set_page_config(
+        page_title="Satellighte Demo Page",
+        page_icon="📡",
+        layout="centered",
+        initial_sidebar_state="expanded",
+        menu_items={
+            "Get Help": "https://canturan10.github.io/satellighte/",
+            "About": "Satellite Image Classification",
+        },
+    )
+
     st.title("Satellighte Demo Page")
+
+    url = "https://raw.githubusercontent.com/canturan10/satellighte/master/src/satellighte.png?raw=true"
+    satellighte = Image.open(requests.get(url, stream=True).raw)
+
+    st.sidebar.image(satellighte, width=100)
+    st.sidebar.title("Satellighte")
+    st.sidebar.caption(sat.__description__)
+
     st.write(
         "**Satellighte** is an image classification library  that consist state-of-the-art deep learning methods. It is a combination of the words **'Satellite'** and **'Light'**, and its purpose is to establish a light structure to classify satellite images, but to obtain robust results."
     )
 
-    url = "https://raw.githubusercontent.com/canturan10/satellighte/master/src/satellighte.png?raw=true"
-    satellighte = Image.open(requests.get(url, stream=True).raw)
-    st.sidebar.image(satellighte, width=150)
-    st.sidebar.title("Satellighte")
-    st.sidebar.caption(f"Version `{sat.__version__}`")
+    st.sidebar.caption(f"Version: `{sat.__version__}`")
+    st.sidebar.caption(f"License: `{sat.__license__}`")
+    st.sidebar.caption(sat.__copyright__)
 
-    uploaded_file = st.sidebar.file_uploader(
-        "", type=["png", "jpg", "jpeg"], accept_multiple_files=False
-    )
-
-    st.sidebar.write(
-        "[Find sample images on Satellighte.](https://github.com/canturan10/satellighte/tree/master/src/eurosat_samples/)"
-    )
-
-    selected_model = st.sidebar.selectbox(
+    selected_model = st.selectbox(
         "Select model",
         sat.available_models(),
     )
-    selected_version = st.sidebar.selectbox(
+    selected_version = st.selectbox(
         "Select version",
         sat.get_model_versions(selected_model),
     )
 
+    model = sat.Classifier.from_pretrained(selected_model, selected_version)
+    model.eval()
+
+    uploaded_file = st.file_uploader(
+        "", type=["png", "jpg", "jpeg"], accept_multiple_files=False
+    )
+
     if uploaded_file is None:
-        # Default image.
-        url = "https://raw.githubusercontent.com/canturan10/satellighte/master/src/eurosat_samples/HerbaceousVegetation.jpg?raw=true"
+        st.write("Sample Image")
+        # Sample image.
+        url = f"https://raw.githubusercontent.com/canturan10/satellighte/master/src/eurosat_samples/{random_sample}?raw=true"
         image = Image.open(requests.get(url, stream=True).raw)
 
     else:
         # User-selected image.
         image = Image.open(uploaded_file)
 
-    st.write("### Inferenced Image")
     image = np.array(image.convert("RGB"))
     FRAME_WINDOW = st.image([], use_column_width=True)
 
@@ -60,4 +79,14 @@ def main():
 
 
 if __name__ == "__main__":
+    samples = [
+        "AnnualCrop.jpg",
+        "Forest.jpg",
+        "HerbaceousVegetation.jpg",
+        "PermanentCrop.jpg",
+        "River.jpg",
+    ]
+    random.seed(datetime.now())
+    random_sample = samples[random.randint(0, len(samples) - 1)]
+
     main()
